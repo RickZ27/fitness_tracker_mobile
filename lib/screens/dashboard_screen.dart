8295430f6584b06f34234967077f8a4c79844065
Umanga Nayaju <153262197+RickZ27@users.noreply.github.com>
2025-12-19 12:03:40 +0545

    app theme

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,10 +1,58 @@
+import 'package:fitness_tracker/screens/navbarscreen/bmi_screen.dart';
+import 'package:fitness_tracker/screens/navbarscreen/home_screen.dart';
+import 'package:fitness_tracker/screens/navbarscreen/profile_screen.dart';
+import 'package:fitness_tracker/screens/navbarscreen/timer_screen.dart';
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    BmiScreen(),
+    TimerScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _screens[_currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.monitor_weight),
+            label: "BMI",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer),
+            label: "Timer",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
   }
 }
