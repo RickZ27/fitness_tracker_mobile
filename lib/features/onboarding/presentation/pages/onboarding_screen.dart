65990d731dba705ccf1e70e2344c82306559d230
Umanga Nayaju <153262197+RickZ27@users.noreply.github.com>
2026-01-03 00:24:54 +0545

    final

--- lib/features/onboarding/presentation/pages/onboarding_screen.dart
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -1,4 +1,4 @@
-import 'package:fitness_tracker/features/auth/presentation/pages/login_screen.dart';
+import 'package:fitness_tracker/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 
